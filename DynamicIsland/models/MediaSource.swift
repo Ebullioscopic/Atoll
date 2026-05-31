762c446c8f7cb84826c9713a3446aad65648894d
bobby-claw <bobby-claw@users.noreply.github.com>
2026-05-31 13:04:43 -0500

    feat: All Music multi-source media mode (PR #405)
    - Add MediaSource model for tracking multiple playing media apps
    - Add MediaControllerType.all case with smart auto-switching
    - NowPlayingController tracks all active sources with 30s stale cleanup
    - MusicManager: secondarySources, promoteSource(), rebuildSecondarySources()
    - Add MultiSourceMediaList and MediaSourceSelectorPopover UI views
    - Targeted playback control via PID-based MediaRemote commands

---
+++ DynamicIsland/models/MediaSource.swift
@@ -0,0 +1,35 @@
+/*
+ * Atoll (DynamicIsland)
+ * Copyright (C) 2024-2026 Atoll Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import AppKit
+import Foundation
+
+/// Represents a detected media source in "All Music" mode.
+struct MediaSource: Identifiable, Equatable {
+    let id: String // bundleID
+    var bundleIdentifier: String
+    var title: String
+    var artistName: String
+    var albumArt: NSImage?
+    var isPlaying: Bool
+    var lastUpdated: Date
+
+    static func == (lhs: MediaSource, rhs: MediaSource) -> Bool {
+        lhs.id == rhs.id
+    }
+}
