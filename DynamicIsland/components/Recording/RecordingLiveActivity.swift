dcfc71f9e7ec1fed6425024f252531a606d4a89e
delli <davidedelellis07@gmail.com>
2026-08-20 14:13:06 +0200

    Nudge recording HUD content lower

--- DynamicIsland/components/Recording/RecordingLiveActivity.swift
+++ DynamicIsland/components/Recording/RecordingLiveActivity.swift
@@ -177,7 +177,7 @@ struct RecordingLiveActivity: View {
         }
         .padding(.leading, 35)
         .padding(.trailing, 40)
-        .padding(.top, 25)
+        .padding(.top, 29)
         .padding(.bottom, 20)
     }
 
